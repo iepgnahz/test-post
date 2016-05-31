fba16d660b47ed5f28a75fe3db89c46c0bf0f1e2
zhangpei <13488233237@163.com>
2016-05-31 20:38:40 +0800

    finish collect_same_elements

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,20 @@
 
 function collect_same_elements(collection_a, object_b) {
  //在此处写代码
+  var length1 = collection_a.length;
+  var length2 = object_b.value.length;
+  var s = "";
+  var arr = [];
+  for(var i = 0; i < length1; i++){
+    s = collection_a[i].key
+    for(var j = 0; j < length2; j++){
+      if(s == object_b.value[j]){
+        arr.push(s);
+        break;
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = collect_same_elements;
